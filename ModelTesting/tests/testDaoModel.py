204b707551706e40674cbf0b12fed04d3791788f
Abhay Kanwasi <djoker682@gmail.com>
2023-11-21 15:12:50 +0530

    function tested

--- ModelTesting/tests/testDaoModel.py
+++ ModelTesting/tests/testDaoModel.py
@@ -8,10 +8,22 @@ class ModelDao:
         This function will not take any parameters. It will give    you all enable categories with atleast one enable service.
 
         - disassociate_category_from_service(service, category)
-        In this function 2 parameters are needed which will be the instances of the main model. You need to pass them in this function and it will disassociate given category from the given service if both exists.
+        In this function, 2 parameters are needed which will be the instances of the main model. You need to pass them in this function and it will disassociate given category from the given service if both exists.
         
         - get_services_with_enable_categories()
         This function will give you all the services who have active categories in it.
+
+        - get_all_categories_along_with_the_numbers_of_services_associated()
+        This function will get all categories along with all the services count that are associated with category.
+
+        # Another method can be used
+        cateories_with_count = Category.objects.annotate(service_count = Count("services"))
+        print(cateories_with_count.values_list())
+        for category in cateories_with_count:
+        print(f"Category : {category.name} | Number of Services : {category.service_count}")
+
+        - get_all_services_along_with_the_number_of_categories_associated() 
+        This function will get all the services along with all the categories count that are associated with service.
 
     """
     def get_enable_categories_with_atleast_one_enabled_service():
@@ -26,22 +38,22 @@ class ModelDao:
         services = Services.objects.filter(categories__is_active = True).distinct()
         return services 
 
-    # Terminal Input 
-    # service1 = Services.objects.get("Service1")
-    # update_the_details_of_service(service1)
+    def get_all_categories_along_with_the_numbers_of_services_associated():
+        categories = Category.objects.all()
+        count = 0
+        for category in categories:
+            service_count_in_categories = category.services.count()
+            print(f"Categories {category.name} | Associated Services {service_count_in_categories}")
+            if service_count_in_categories != 0:
+                count += 1
+        return count
 
-def get_all_categories_along_with_the_numbers_of_services_associated():
-    #  cateories_with_count = Category.objects.annotate(service_count = Count("services"))
-    #  print(cateories_with_count.values_list())
-    #  for category in cateories_with_count:
-    #     print(f"Category : {category.name} | Number of Services : {category.service_count}")
-    
-    categories = Category.objects.all()
-    print(categories.query)
-    for category in categories:
-        service_count_in_category = category.services.count()
-        print(f"Categories {category.name} | Associated Services {service_count_in_category}")
-
-
-
-    
+    def get_all_services_along_with_the_number_of_categories_associated():
+        services = Services.objects.all()
+        count = 0
+        for service in services:
+            category_count_in_services = service.categories.count()
+            print(f"Service {service.name} | Associated Categories {category_count_in_services}")
+            if category_count_in_services != 0:
+                count += 1
+        return count
--- ModelTesting/tests/test_modeltesting.py
+++ ModelTesting/tests/test_modeltesting.py
@@ -20,39 +20,28 @@ class ModelTest(TestCase):
     def test_create_category(self):
         print("\nTesting category creation")
         category1 = Category.objects.create(name="test_create_category")
-        try:
-            self.assertTrue(Category.objects.filter(name="test_create_category").exists())
-            self.assertFalse(Category.objects.filter(name="test_create_category", is_active = True).exists())
-            self.assertEqual(category1.name, "test_create_category")
-            print("Test passed !")
-        except AssertionError as error:
-            print(f"Test failed due to: {error}")
+        self.assertTrue(Category.objects.filter(name="test_create_category").exists())
+        self.assertFalse(Category.objects.filter(name="test_create_category", is_active = True).exists())
+        self.assertEqual(category1.name, "test_create_category")
+        print("Test passed !")
         
     def test_create_service(self):
         print("\nTesting service creation")
         service1 = Services.objects.create(name = "test_create_service", description = "test_create_service description", is_active = True)
-        try:
-            self.assertTrue(Services.objects.filter(name = "test_create_service", is_active = True).exists())
-            self.assertFalse(Services.objects.filter(name = "test_create_service", is_active = False).exists())
-            self.assertEqual(service1.description, "test_create_service description")
-            self.assertEqual(service1.name, "test_create_service")
-
-            print("Test passed !")
-        except AssertionError as error:
-            print(f"Test failed due to : {error}")
+        self.assertTrue(Services.objects.filter(name = "test_create_service", is_active = True).exists())
+        self.assertFalse(Services.objects.filter(name = "test_create_service", is_active = False).exists())
+        self.assertEqual(service1.description, "test_create_service description")
+        self.assertEqual(service1.name, "test_create_service")
+        print("Test passed !")   
 
     def test_add_category1_and_category2_to_service1(self):
         print("\nTesting addition of category into service1")
         self.service1.categories.add(self.category1, self.category2)
-        try:
-            self.assertTrue(self.service1.categories.filter(name = "Category1").exists())
-            self.assertTrue(self.service1.categories.filter(name = "Category2").exists())
-
-            self.assertEqual(1, self.service1.categories.filter(name = "Category1").count())
-            self.assertEqual(1, self.service1.categories.filter(name = "Category2").count())
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
+        self.assertTrue(self.service1.categories.filter(name = "Category1").exists())
+        self.assertTrue(self.service1.categories.filter(name = "Category2").exists())
+        self.assertEqual(1, self.service1.categories.filter(name = "Category1").count())
+        self.assertEqual(1, self.service1.categories.filter(name = "Category2").count())
+        print("Test passed !")
 
     def test_remove_category1_from_service1(self):
         print("\nTesting category1 remove from service1")
@@ -60,56 +49,41 @@ class ModelTest(TestCase):
         category_to_remove = self.category1
         service1.categories.add(category_to_remove)
         service1.categories.remove(category_to_remove)
-        try:
-            self.assertFalse(service1.categories.filter(name="Category1").exists())
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
+        self.assertFalse(service1.categories.filter(name="Category1").exists())
+        print("Test passed !")
 
     def test_add_category_2_3_to_service_with_categories_1_2(self):
         print("\nTesting addition of category2 and category3 to service1 with categories 1 and 2")
         self.service1.categories.add(self.category2, self.category3)
-        try:
-            self.assertTrue(self.service1.categories.filter(name="Category2").exists())
-            self.assertTrue(self.service1.categories.filter(name="Category3").exists())
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
-
+        self.assertTrue(self.service1.categories.filter(name="Category2").exists())
+        self.assertTrue(self.service1.categories.filter(name="Category3").exists())
+        print("Test passed !")
+        
     def test_remove_category_2_from_service1(self):
         print("\nTesting remove of category2 from service1")
         service1 = self.service1
         category_to_remove = self.category2
         service1.categories.add(category_to_remove)
         service1.categories.remove(category_to_remove)
-        try:
-            self.service1.categories.remove(category_to_remove)
-            self.assertFalse(self.service1.categories.filter(name="Category2").exists())
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
+        self.service1.categories.remove(category_to_remove)
+        self.assertFalse(self.service1.categories.filter(name="Category2").exists())
+        print("Test passed !")
     
     def test_categories_with_is_active_false_exits(self):
         print("\nTesting for searching is_active false in category")
-        try:
-            if Category.objects.filter(is_active=False).exists():
-                self.assertTrue(Category.objects.filter(is_active=False).exists())
-            else:
-                print("No categories with active status false")
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
+        if Category.objects.filter(is_active=False).exists():
+            self.assertTrue(Category.objects.filter(is_active=False).exists())
+        else:
+            print("No categories with active status false")
+        print("Test passed !")
     
     def test_get_enable_categories_with_atleast_one_enabled_service(self):
         print("\nTesting for getting enable categories with atleast one enable service")
         categories = ModelDao.get_enable_categories_with_atleast_one_enabled_service()
-        try:
-            self.assertIsNotNone(categories)
-            category_count = categories.count()
-            self.assertEquals(categories.count(), category_count)
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
+        self.assertIsNotNone(categories)
+        category_count = categories.count()
+        self.assertEquals(categories.count(), category_count)
+        print("Test passed !")
 
     def test_disassociate_category_from_service(self):
         print("\nTesting of disassociating category from service")
@@ -119,14 +93,18 @@ class ModelTest(TestCase):
         service_with_categories_before = service1.categories.all().count()
         ModelDao.disassociate_category_from_service(service1, category1)
         service_with_categories_after = service1.categories.all().count()
-        try:
-            self.assertNotEqual(service_with_categories_after, service_with_categories_before)
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
+        self.assertNotEqual(service_with_categories_after, service_with_categories_before)
+        print("Test passed !")
 
     def test_update_the_details_of_service1(self):
         print("\nTesting updating details of service1")
+
+        with self.assertRaises(Services.DoesNotExist):
+            service1 = Services.objects.get(name = "Unknown Service")
+        
+        with self.assertRaises(Category.DoesNotExist):
+            category1 = Category.objects.get(name = "Unknown Category")
+
         service1 = self.service1
         new_name = "updated service1 name"
         new_description = "updated service1 description" 
@@ -134,83 +112,60 @@ class ModelTest(TestCase):
         service1.description = new_description
         service1.is_active = False
         service1.save()
-        # Check if the details have been updated
-        try:
-            self.assertEqual(self.service1.name, new_name)
-            self.assertEqual(self.service1.description, new_description)
-            self.assertEqual(self.service1.is_active, False) 
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
+        self.assertEqual(self.service1.name, new_name)
+        self.assertEqual(self.service1.description, new_description)
+        self.assertEqual(self.service1.is_active, False) 
+        print("Test passed !")
+
     
     def test_get_services_with_enable_categories(self):
         print("\nTesting for all the services who have active categories in them")
-        try:
-            services = ModelDao.get_services_with_enable_categories()
-            self.assertIsNotNone(services)
-            print("Test passed !")
-        except AssertionError as error:
-            print(f'Test failed due to : {error}')
-    
+        services = ModelDao.get_services_with_enable_categories()
+        self.assertIsNotNone(services)
+        print("Test passed !")
+        
     def test_category_and_service_exists(self):
         print("\nTesting category and services existance")
         # Test Case 1: Service and Category do not exist
-        service_name = "test_create_service"
-        service_description = "Test description"
-        category_name = "nonexistent"
+        service_name = "Service2"
+        service_description = "Service2"
+        category_name = "Category1"
     
-        try:
-            # Attempt to create a service with a nonexistent category
-            with self.assertRaises(Category.DoesNotExist):
-                category1 = Category.objects.create(name = category_name)
-            with self.assertRaises(Services.DoesNotExist):
-                service1 = Services.objects.create(name=service_name, description=service_description, category=Category.objects.get(name=category_name))
-            print("Testpassed!")
-        except AssertionError as error:
-            print(f"Test2 - Case 1 failed: {error}")
+        # Attempt to create a service with a nonexistent category
+        with self.assertRaises(Category.DoesNotExist):
+            category1 = Category.objects.get(name = "something")
+
+        with self.assertRaises(Services.DoesNotExist):
+            service1 = Services.objects.get(name="something")
+        print("Test1 passed!")
 
         # Test Case 2: Service and Category both exist
-        try:
-            category1 = self.category1
-            service2 = self.service1
+        category1 = self.category1
+        service2 = self.service2
 
-            self.assertTrue(Services.objects.filter(name=service_name).exists())
-            self.assertTrue(Category.objects.filter(name=category1.name).exists())
-            self.assertEqual(service2.name, service_name)
-            self.assertEqual(service2.description, service_description)
-            print("Test passed!")
-        except AssertionError as error:
-            print(f"Test2 - Case 2 failed: {error}")
+        self.assertTrue(Services.objects.filter(name=service2.name).exists())
+        self.assertTrue(Category.objects.filter(name=category1.name).exists())
+        self.assertEqual(service2.name, service_name)
+        self.assertEqual(category1.name, category_name)
+        self.assertEqual(service2.description, service_description)
+        print("Test2 passed!")
 
+    def test_get_all_categories_along_with_the_numbers_of_services_associated(self):
+        print("\nTesting all categories getting the associated services count")
+        self.service1.categories.add(self.category1, self.category2, self.category3)
+        count = ModelDao.get_all_categories_along_with_the_numbers_of_services_associated()
+        self.assertEqual(3, count)
+        print("Test passed !")
+
+    def test_get_all_services_along_with_the_number_of_categories_associated(self):
+        print("\nTesting all services getting the associated categories count")
+        self.service1.categories.add(self.category1, self.category3)
+        self.service2.categories.add(self.category1, self.category5, self.category3)
+        self.service4.categories.add(self.category3, self.category4, self.category1, self.category5)
+        count = ModelDao.get_all_services_along_with_the_number_of_categories_associated()
+        self.assertEqual(3, count)
+        print("Test passed !")
     
-    def test_disassociate_category_from_service_doesnotexist(self):
-        print("\nTesting disassociate category from service")
-        try:
-            service1 = Services.objects.get(name = "something")
-            category1 = self.category1
-            ModelDao.disassociate_category_from_service(service1, category1)
-            print("Test1 passed !s")
-        except Services.DoesNotExist as error:
-            print(f"Error due to {error}")
-        
-        try:
-            service1 = self.service1
-            category1 = Category.objects.get(name = "something")
-            ModelDao.disassociate_category_from_service(service1, category1)
-            print("Test2 passed !")
-        except Category.DoesNotExist as error:
-            print(f"Error due to {error}")
-
-        try:
-            service1 = Services.objects.get(name = "something")
-            category1 = Category.objects.get(name = "something")
-            ModelDao.disassociate_category_from_service(service1, category1)
-            print("Test3 passed !")
-        except Exception as e:
-            print(f'Error due to missing parameters ! {e}')
-        
-
-
     def tearDown(self):
         Category.objects.all().delete()
         Services.objects.all().delete()
