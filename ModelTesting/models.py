f220b1b37495006af31cc87a3dfa3ca5142fc0a8
Abhay Kanwasi <djoker682@gmail.com>
2023-11-22 11:57:24 +0530

    new model added

--- ModelTesting/models.py
+++ ModelTesting/models.py
@@ -7,6 +7,14 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Form(models.Model):
+    name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 class Pricing(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=1)
     is_active = models.BooleanField(default=False)
@@ -19,6 +27,7 @@ class Services(models.Model):
     description = models.CharField(max_length=30)
     pricing = models.OneToOneField(Pricing, on_delete=models.CASCADE, default=0.0)
     categories = models.ManyToManyField(Category, related_name="services")
+    form = models.ManyToManyField(Form, related_name="services")
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
